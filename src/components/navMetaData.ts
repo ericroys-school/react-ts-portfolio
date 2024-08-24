30c26f94ec07947314119c1931b4fcccd4cd87d6
eric.roys@gmail.com <eric.roys@gmail.com>
2024-08-23 20:26:22 -0500

    feature: add the meta data to define nav links
    -add name, link, and icon for each nav item

---
+++ src/components/navMetaData.ts
@@ -0,0 +1,12 @@
+import { StyledNavLink } from '../components/navbar';
+import { HiHome } from 'react-icons/hi2';
+import { FaUncharted } from 'react-icons/fa';
+import { FaRocket } from 'react-icons/fa';
+import { MdMenuBook } from 'react-icons/md';
+
+export const links: StyledNavLink[] = [
+  { name: 'Home', href: '/home', icon: HiHome },
+  { name: 'Other', href: '/notfound', icon: FaUncharted },
+  { name: 'About Us', href: '/', icon: FaRocket },
+  { name: 'Resume', href: '/resume', icon: MdMenuBook },
+];
