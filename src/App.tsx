64fccd780925c90bb8a8f0c30fcfe741df3fa03d
eric.roys@gmail.com <eric.roys@gmail.com>
2024-08-23 20:19:05 -0500

    feature: stub in the header and routes
    -add routes for each of the nav links
    -add the header component with the nav links

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home } from './pages/home';
+import { NotFound } from './pages/notfound';
+import { AboutPage } from "./pages/about";
+import { Contact } from './pages/contact'
+import { NavHeader } from './components/header';
 
 
-function App() {
+const App = () => {
 
   return (
-    <>
-    </>
+    <BrowserRouter>
+    <NavHeader />
+    <Routes>
+        <Route path="/" element={<AboutPage />} />
+        <Route path="/notfound" element={<NotFound />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/contact" element={<Contact/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
