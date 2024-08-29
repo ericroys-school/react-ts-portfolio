751c7e2d6d681ed9952d5909729fe8a4b60b5671
eric.roys@gmail.com <eric.roys@gmail.com>
2024-08-29 13:58:52 -0500

    organization: move data into the data dir

--- src/data/navMetaData.ts
+++ src/data/navMetaData.ts
@@ -4,7 +4,7 @@ import { FaRocket } from 'react-icons/fa';
 import { MdMenuBook } from 'react-icons/md';
 
 export const links: StyledNavLink[] = [
-  { name: 'Portfolio', href: '/home', icon: FaUncharted },
+  { name: 'Portfolio', href: '/portfolio', icon: FaUncharted },
   { name: 'About Us', href: '/', icon: FaRocket },
   { name: 'Resume', href: '/resume', icon: MdMenuBook },
 ];
