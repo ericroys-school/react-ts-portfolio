262652cbf7f9200ac2537b518e3dcc70869f10e8
eric.roys@gmail.com <eric.roys@gmail.com>
2024-08-29 15:10:00 -0500

    refactor: remove original home page and replace with portfolio

---
+++ src/pages/portfolio.tsx
@@ -0,0 +1,12 @@
+import { ProjectList } from "../components/projects"
+
+export const Portfolio = () =>{
+    return (
+        <>
+        <div className="flex flex-col items-center">
+        <ProjectList />
+        </div>
+        </>
+    )
+}
+
