85b47c3228265d5cede4e00571c247224504595b
eric.roys@gmail.com <eric.roys@gmail.com>
2024-08-29 13:56:39 -0500

    feature: change header rocket and text container sizing

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,17 +1,17 @@
-import { links } from './navMetaData';
+import { MdRocketLaunch } from 'react-icons/md';
+import { links } from '../data/navMetaData';
 import { StyledNav } from './navbar';
-import { FaRocket } from 'react-icons/fa';
 
 export const NavHeader = () => {
   return (
     <>
-      <div className='bg-redish flex h-30 items-center bg-cyan-600 justify-between'>
-        <div className='flex w-1/4'>
-          <FaRocket id="rocket" size={20} className='rotate-45'/> 
+      <div className='bg-redish flex h-30 items-center justify-between'>
+        <div className='flex w-1/2'>
+          <MdRocketLaunch  id="rocket" size={22} className='rotate-45 fill-blueish-dark'/> 
           <h1
           className='font-bold bg-bluish font-special m-4 text-center text-4xl text-wrap
-            w-1/2 bg-slate-200 p-4 rounded-md border-slate-600 border-solid border-t 
-            border-e-0 border-l-4 shadow-2xl min-w-min '>
+            w-3/4 p-4 rounded-md border-bluish-dark border-solid border-t 
+            border-e-0 border-l-4 shadow-2xl '>
           Eric Roys
                    
         </h1>
