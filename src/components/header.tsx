a2e8b65a83c7401a3a5347c5b252d32d44cb4735
eric.roys@gmail.com <eric.roys@gmail.com>
2024-08-30 14:01:06 -0500

    styling: give the name box a gradient fill and the header a shadow

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,13 +5,13 @@ import { StyledNav } from './navbar';
 export const NavHeader = () => {
   return (
     <>
-      <div className='bg-redish flex h-30 items-center justify-between'>
+      <div className='bg-redish flex h-30 items-center justify-between shadow-lg shadow-greenish-light'>
         <div className='flex w-1/2'>
           <MdRocketLaunch  id="rocket" size={22} className='rotate-45 fill-blueish-dark'/> 
           <h1
-          className='font-bold bg-bluish font-special m-4 text-center text-4xl text-wrap
+          className='font-bold bg-gradient-to-r from-bluish to-greenish-light font-special m-4 text-center text-4xl text-wrap
             w-3/4 p-4 rounded-md border-bluish-dark border-solid border-t 
-            border-e-0 border-l-4 shadow-2xl '>
+            border-e-0 border-l-4'>
           Eric Roys
                    
         </h1>
