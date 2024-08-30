4c1707786e14636b911d1d2ee5d7d07dbf395ad9
eric.roys@gmail.com <eric.roys@gmail.com>
2024-08-30 14:08:44 -0500

    feature: finish the stubbed in StyledIconListCard component

--- src/lib/card-styledIconList.tsx
+++ src/lib/card-styledIconList.tsx
@@ -11,10 +11,15 @@ export type Props = {
 export const StyledIconListCard = ({ data }: Props) => {
   let { headerTxt, iconTexts } = data;
   let iclass = 'fill-blueish-dark'
+  let tclass = 'font-extrabold text-bluish-dark'
   return (
-    <div className='flex flex-col'>
-      {headerTxt}
-      {iconTexts.map((icotxt, idx) => <IconWithText key={idx} icon={icotxt.icon} iconClass={iclass} text={icotxt.text}/>)}
+    <div className='flex flex-col w-full bg-greenish-light'>
+        <div className='bg-pinkish rounded-t-md font-extrabold text-bluish-dark shadow-md shadow-black'>
+      {headerTxt}</div>
+      <div className='flex flex-col border-pinkish border-2 pl-4 pt-2 pb-2 h-full'>
+      {iconTexts.map((icotxt, idx) => 
+      <IconWithText key={idx} icon={icotxt.icon} iconClass={iclass} text={icotxt.text} txtClass={tclass}/>)}
+      </div>
     </div>
   );
 };
